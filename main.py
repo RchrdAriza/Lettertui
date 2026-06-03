b1f9600aebdb7015425d6f4254909144e3eb91a3
RchrdAriza <rchrdalv@gmail.com>
2026-06-03 14:13:51 -0500

    mejorado el tema de las imagenes

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ from PIL import Image
 from io import BytesIO
 from textual.app import App, ComposeResult
 from textual.widget import Widget
-from textual_imageview.viewer import ImageViewer
 from textual.containers import Grid
 from textual.widgets import Header, Footer, Static
+from textual.containers import Center
 from API.tmdb import search_movies
+from textual_image.widget import Image as textual_image
 
-data = search_movies("inception")
+data = search_movies("superman")
 pelicula = data["results"][0]
 poster_path = pelicula["poster_path"]
 
 
-class PortadaImagen(Static):
-
-    def compose(self) -> ComposeResult:
-        placeholder = Image.new("RGB", (1, 1))
-        yield ImageViewer(placeholder)
-
+class Poster(Static):
     async def on_mount(self) -> None:
-        self.loading = True
         async with httpx.AsyncClient() as client:
             response = await client.get(f"https://image.tmdb.org/t/p/w300{poster_path}")
-            imagen = Image.open(BytesIO(response.content))
+            pilimage = Image.open(BytesIO(response.content))
 
-        viewer = self.query_one(ImageViewer)
-        await viewer.remove()
-        await self.mount(ImageViewer(imagen))
-        self.loading = False
+            poster = textual_image(pilimage)
+
+            await self.mount(poster)
 
 
 class Portada(Widget):
@@ -37,7 +31,7 @@ class Portada(Widget):
         self.border_subtitle = "Portada"
 
     def compose(self) -> ComposeResult:
-        yield PortadaImagen()
+        yield Poster()
 
 
 class Info(Static):
