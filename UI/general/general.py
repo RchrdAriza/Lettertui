999bf9ede2ca3bb9bd1a258822c324ff6ea377fe
RchrdAriza <rchrdalv@gmail.com>
2026-06-03 14:57:18 -0500

    separé un poco la logica

---
+++ UI/general/general.py
@@ -0,0 +1,50 @@
+from textual_image.widget import Image as textual_image
+from textual.widgets import Static
+import httpx
+from PIL import Image
+from io import BytesIO
+from API.tmdb import search_movies
+from textual.app import ComposeResult
+from textual.widget import Widget
+
+data = search_movies("superman")
+pelicula = data["results"][0]
+poster_path = pelicula["poster_path"]
+
+
+class Poster(Static):
+    async def on_mount(self) -> None:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"https://image.tmdb.org/t/p/w300{poster_path}")
+            pilimage = Image.open(BytesIO(response.content))
+
+            poster = textual_image(pilimage)
+
+            await self.mount(poster)
+
+
+class Portada(Widget):
+
+    def on_mount(self) -> None:
+        self.border_subtitle = "Portada"
+
+    def compose(self) -> ComposeResult:
+        yield Poster()
+
+
+class Info(Static):
+
+    def on_mount(self) -> None:
+        self.border_subtitle = "Info"
+
+    def compose(self) -> ComposeResult:
+        yield Static("Info")
+
+
+class Metadata(Static):
+
+    def on_mount(self) -> None:
+        self.border_title = "Metadata"
+
+    def compose(self) -> ComposeResult:
+        yield Static("Metadata")
--- main.py
+++ main.py
@@ -1,55 +1,8 @@
-import httpx
-from PIL import Image
-from io import BytesIO
 from textual.app import App, ComposeResult
 from textual.widget import Widget
 from textual.containers import Grid
-from textual.widgets import Header, Footer, Label, Static, TabPane, TabbedContent
-from textual.containers import Center
-from API.tmdb import search_movies
-from textual_image.widget import Image as textual_image
-
-data = search_movies("superman")
-pelicula = data["results"][0]
-poster_path = pelicula["poster_path"]
-
-
-class Poster(Static):
-    async def on_mount(self) -> None:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(f"https://image.tmdb.org/t/p/w300{poster_path}")
-            pilimage = Image.open(BytesIO(response.content))
-
-            poster = textual_image(pilimage)
-
-            await self.mount(poster)
-
-
-class Portada(Widget):
-
-    def on_mount(self) -> None:
-        self.border_subtitle = "Portada"
-
-    def compose(self) -> ComposeResult:
-        yield Poster()
-
-
-class Info(Static):
-
-    def on_mount(self) -> None:
-        self.border_subtitle = "Info"
-
-    def compose(self) -> ComposeResult:
-        yield Static("Info")
-
-
-class Metadata(Static):
-
-    def on_mount(self) -> None:
-        self.border_title = "Metadata"
-
-    def compose(self) -> ComposeResult:
-        yield Static("Metadata")
+from textual.widgets import Header, Footer, Label, TabPane, TabbedContent
+from UI.general.general import Portada, Info, Metadata
 
 
 class Pestanas(Widget):
